bc1a5c8b8113fbe283897fc6313ce9fd27cd76d9
paolospoto <paolo.spoto@gmail.com>
2024-02-05 11:34:57 +0100

    first setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
+import "@mantine/core/styles.css";
 import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider defaultColorScheme="dark">
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
